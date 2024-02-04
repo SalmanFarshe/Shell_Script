c7583d7f96838fc9f7afc759ef18aad5b4423dfb
Salman Farshe <salman.zihad@involve.ai>
2024-02-04 22:45:51 +0600

    User Info

---
+++ useInfo.sh
@@ -0,0 +1,12 @@
+read -p "Frist Name : " first_name
+read -p "Last Name : " last_name
+read -p "Enter Age : " age
+read -p "NID Number : " nid_number
+read -p "Phone Number : " phone_number
+read -p "Marital Status : "  maritul_status
+echo $first_name
+echo $last_name
+echo $age
+echo $nid_number
+echo $phone_number
+echo $maritul_status
